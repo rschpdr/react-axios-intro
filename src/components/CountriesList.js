8a8f9f3876d4b025b009ce912e4f0dff09a0c9ba
rschpdr <pedro.resch@gmail.com>
2020-07-29 15:33:43 -0300

    Switched useState for useReducer

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import api from "../apis/countries";
@@ -6,31 +6,48 @@ import api from "../apis/countries";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
 
+function countriesReducer(state, action) {
+  switch (action.type) {
+    case "FETCH_START":
+      return { ...state, loading: true };
+    case "FETCH_END":
+      return { ...state, countries: [...action.payload], loading: false };
+    case "FETCH_ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      throw new Error("Unhandled action type");
+  }
+}
+
 const CountriesList = () => {
   // Inicializando nosso state
-
-  const [countries, setCountries] = useState([]);
-  const [error, setError] = useState(false);
+  const [state, dispatch] = useReducer(countriesReducer, {
+    error: false,
+    loading: false,
+    countries: [],
+  });
 
   // Definicoes de efeitos colaterais
   useEffect(() => {
     (async function getCountries() {
       try {
+        // Seta o state de loading para true, para exibir nosso Spinner, indicando ao usuario que algo esta acontecendo
+        dispatch({ type: "FETCH_START" });
         // api e uma instancia do Axios pre-configurada com uma URL padrao
         const response = await api.get();
 
         // Atualize a lista de paises com o resultado da chamada HTTP
-        setCountries([...response.data]);
+        dispatch({ type: "FETCH_END", payload: response.data });
       } catch {
         // Atualize o estado do componente para erro, para exibir a mensagem de erro
-        return setError(true);
+        dispatch({ type: "FETCH_ERROR" });
       }
     })();
   }, []);
 
   // Funcao para renderizar mensagem de erro
   function renderError() {
-    return error ? <ErrorMessage /> : null;
+    return state.error ? <ErrorMessage /> : null;
   }
 
   // Retornando JSX
@@ -39,9 +56,9 @@ const CountriesList = () => {
       <h1>List of countries</h1>
       {renderError()}
       {/* Se countries tiver elementos, renderize uma lista. Caso contrario, renderize um feedback pro usuario de carregamento */}
-      {countries.length ? (
+      {!state.loading ? (
         <ul>
-          {countries.map((country, i) => (
+          {state.countries.map((country, i) => (
             <li key={i}>
               <Link to={`/country/${country.name.common}`}>
                 {country.name.official}
